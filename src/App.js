37e141310c1f2f23a2507252c75bb8da43e94017
melodyWxy <18210711176@163.com>
2021-09-09 00:49:20 +0800

    feat: +antd

--- src/App.js
+++ src/App.js
@@ -1,39 +1,60 @@
+import { useSchemeJSON, useSortAble } from "./effect";
+import { baseComponents } from "./config/BaseComponents";
 
-import { useSchemeJSON, useSortAble } from './effect'
-import {baseComponents} from './config/BaseComponents';
-
-import styles from './App.module.css';
-
+import { Form } from "antd";
+import styles from "./App.module.css";
 
 function App() {
-  const { json, updateJSONAfterMove, updateJSONByEditor, updateJSONAfterSort } = useSchemeJSON();
-  const {
-    baseComponentWrapRef,
-    canvarsWrapRef
-  } = useSortAble(updateJSONAfterMove, updateJSONAfterSort);
-  console.log('json', json);
+  const { json, updateJSONAfterMove, updateJSONByEditor, updateJSONAfterSort } =
+    useSchemeJSON();
+  const { baseComponentWrapRef, canvarsWrapRef } = useSortAble(
+    updateJSONAfterMove,
+    updateJSONAfterSort
+  );
+  console.log("json", json);
+  // TODO
+  const renderCanvarsChildren = () => {
+    const items = json.children.map((item) => {
+      const target = baseComponents.find((base) => base.type === item.type);
+      const { wrapConfig = {}, Component, initProps } = target || {};
+      return (
+        <Form.Item {...wrapConfig} key={item.key}>
+          <Component {...(initProps || {})} />
+        </Form.Item>
+      );
+    });
+    return (
+      <Form name="demo" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+        {items}
+      </Form>
+    );
+  };
+
   return (
-    <div className={styles['app']}>
-      <div className={styles['baseWrap']}>
+    <div className={styles["app"]}>
+      <div className={styles["baseWrap"]}>
         <div>
-          <div className={styles['base-title']}>
-              基础组件
-          </div>
-          <div className={styles['base-list']} ref={baseComponentWrapRef}>
-            {baseComponents.map(item => (
-              <div className={styles['base-item-com']} data-type={item.key} key={item.key}>
+          <div className={styles["base-title"]}>基础组件</div>
+          <div className={styles["base-list"]} ref={baseComponentWrapRef}>
+            {baseComponents.map((item) => (
+              <div
+                className={styles["base-item-com"]}
+                data-type={item.key}
+                key={item.key}
+              >
                 {item.title}
               </div>
             ))}
           </div>
         </div>
-
-
       </div>
-      <div className={styles['canvarsWrap']} ref={canvarsWrapRef}>
-        {json.children.map(item => (<div key={item.key}>{item.type}</div>))}
+      <div className={styles["canvarsWrap"]} ref={canvarsWrapRef}>
+        {/* {json.children.map((item) => (
+          <div key={item.key}>{item.type}</div>
+        ))} */}
+        {renderCanvarsChildren()}
       </div>
-      <div className={styles['editorWrap']}>编辑区</div>
+      <div className={styles["editorWrap"]}>编辑区</div>
     </div>
   );
 }
--- src/config/BaseComponents.js
+++ src/config/BaseComponents.js
@@ -1,67 +1,74 @@
-export const baseComponents = [{
-    title: '单行输入框',
-    type: 'input',
-    name: 'Input',
-    key: 'input',
-    props: {
-        placeholder: '请输入'
-    },
-    config: {
+import { Input, Select, Radio, Checkbox, Button } from "antd";
 
-    }
-},{
-    title: '下拉选择框',
-    type: 'select',
-    name: 'Select',
-    key: 'select',
-    props: {
-        placeholder: '请输入',
+const RadioGroup = Radio.Group;
+const CheckboxGroup = Checkbox.Group;
+const { TextArea } = Input;
+
+export const baseComponents = [
+  {
+    title: "单行输入框",
+    type: "input",
+    name: "Input",
+    key: "input",
+    initProps: {
+      placeholder: "请输入",
     },
-    config: {
-        
-    }
-},{
-    title: '单选框组',
-    type: 'radio',
-    name: 'Radio',
-    key: 'radio',
-    props: {
-        placeholder: '请输入'
+    config: {},
+    Component: Input,
+  },
+  {
+    title: "下拉选择框",
+    type: "select",
+    name: "Select",
+    key: "select",
+    initProps: {
+      placeholder: "请输入",
     },
-    config: {
-        
-    }
-},{
-    title: '多选框组',
-    type: 'check',
-    name: 'Check',
-    key: 'check',
-    props: {
-        placeholder: '请输入'
+    config: {},
+    Component: Select,
+  },
+  {
+    title: "单选框组",
+    type: "radio",
+    name: "Radio",
+    key: "radio",
+    initProps: {
+      placeholder: "请输入",
     },
-    config: {
-        
-    }
-},{
-    title: '多行文本',
-    type: 'textArea',
-    name: 'TextArea',
-    key: 'textArea',
-    props: {
-        placeholder: '请输入'
+    config: {},
+    Component: RadioGroup,
+  },
+  {
+    title: "多选框组",
+    type: "check",
+    name: "Check",
+    key: "check",
+    initProps: {
+      placeholder: "请输入",
     },
-    config: {
-        
-    }
-},{
-    title: '文字链接',
-    type: 'link',
-    name: 'Link',
-    key: 'link',
-    props: {
-        placeholder: '请输入'
+    config: {},
+    Component: CheckboxGroup,
+  },
+  {
+    title: "多行文本",
+    type: "textArea",
+    name: "TextArea",
+    key: "textArea",
+    initProps: {
+      placeholder: "请输入",
     },
-    config: {
-        
-    }
-},];
+    config: {},
+    Component: TextArea,
+  },
+  {
+    title: "文字链接",
+    type: "link",
+    name: "Link",
+    key: "link",
+    initProps: {
+      placeholder: "请输入",
+    },
+    config: {},
+    Component: Button,
+  },
+];
